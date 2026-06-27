bc6f83ec8705be00672cce55152ecd581cd6130e
AntarGFX <elel.antar@gmail.com>
2026-06-27 08:11:18 -0700

    Upload config.js

---
+++ examv7/config.js
@@ -0,0 +1,14 @@
+/**
+ * config.js — ExamPrep v3
+ * Firebase configuration (real credentials).
+ */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbUVFM6X2yGIEi9iA7EzaEgHLw5_2f9n4",
+  authDomain: "examdibo-bf426.firebaseapp.com",
+  projectId: "examdibo-bf426",
+  storageBucket: "examdibo-bf426.firebasestorage.app",
+  messagingSenderId: "728661434484",
+  appId: "1:728661434484:web:f42e927dd35f4d785b7121",
+  measurementId: "G-XKEJVPG2FW"
+};
